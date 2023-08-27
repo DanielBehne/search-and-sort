f072696209479000e65ec7000170f5ea99301b4b
DanielBehne <daniel.behne@mrjaffesclass.com>
2023-08-26 18:04:37 -0700

    Finished insertion

--- sort/Insertion.java
+++ sort/Insertion.java
@@ -17,12 +17,20 @@ public class Insertion
   public static int[] sort(int[] arr) {
     // Your algorithm goes here!
     for (int i = 1; i < arr.length; i++) {
-        
+        int temp = arr[i];
+        int prev = i-1;
+        while (prev >= 0 && arr[prev] > temp) {
+            arr[i] = arr[prev];
+            arr[prev] = temp;
+            prev--;
+        }
+        arr[prev+1] = temp;
     }
+    return arr;
   }
   
   public static void main(String[] args) {
-    int[] arr = {53,85,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
+    int[] arr = {85,53,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
       31,66,71,32,19,79,58,61,82,89,63,7,4,50,10,48,24,75,19,22,
       73,54,51,25,33,20,52,79,97,70,54,63,49};    
     
