84dae847440bcefd63fca6f31e3d2bb45eaf6436
DanielBehne <daniel.behne@mrjaffesclass.com>
2023-08-28 16:57:15 -0700

    Started selection

--- sort/Selection.java
+++ sort/Selection.java
@@ -16,11 +16,19 @@ public class Selection
    */
   public static int[] sort(int[] arr) {
     // Your algorithm goes here!
-
+    int smallestNum = 1;
+    for (int n = 0; n < arr.length; n++) {
+        if (arr[n] < smallestNum) {
+            arr[n] = smallestNum;
+        } else {
+            n++;
+        }
+    }
+    return arr;
   }
   
   public static void main(String[] args) {
-    int[] arr = {53,85,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
+    int[] arr = {85,53,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
       31,66,71,32,19,79,58,61,82,89,63,7,4,50,10,48,24,75,19,22,
       73,54,51,25,33,20,52,79,97,70,54,63,49};    
     
